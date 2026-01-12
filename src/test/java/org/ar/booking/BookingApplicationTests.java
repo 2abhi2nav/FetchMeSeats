bd8218d9677d50d0941f7012ed57ef87adf828e5
2abhi2nav <abhinavrenji@gmail.com>
2026-01-12 14:07:05 +0530

    Initial commit

---
+++ src/test/java/org/ar/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ar.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
