28b24557f66db5f5089efa5522594109bb1967e4
2abhi2nav <abhinavrenji@gmail.com>
2026-02-22 11:21:39 +0530

    Fix routes in API Gateway

--- apigateway/src/main/java/org/ar/apigateway/route/BookingRoutes.java
+++ apigateway/src/main/java/org/ar/apigateway/route/BookingRoutes.java
@@ -1,26 +1,24 @@
 package org.ar.apigateway.route;
 
-import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RequestPredicates;
-import org.springframework.cloud.gateway.server.mvc.handler.ProxyExchangeHandlerFunctions;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.web.servlet.function.RequestPredicates.path;
 
 @Configuration
 public class BookingRoutes {
 
     @Bean
-    public RouterFunction<ServerResponse> bookingRoutes() {
-        return GatewayRouterFunctions.route("booking-service")
-                .route(
-                        RequestPredicates.POST("/api/v1/booking"),
-                        http("http://localhost:8081/api/v1/booking")
-                )
+    public RouterFunction<ServerResponse> routeToBooking() {
+
+        return route("booking_routes")
+                .route(path("/api/v1/booking/**"), http())
+                .before(uri("http://localhost/8081"))
                 .build();
     }
 }
--- apigateway/src/main/java/org/ar/apigateway/route/InventoryRoutes.java
+++ apigateway/src/main/java/org/ar/apigateway/route/InventoryRoutes.java
@@ -1,38 +1,24 @@
 package org.ar.apigateway.route;
 
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
-import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.web.servlet.function.RequestPredicates.path;
 
 @Configuration
 public class InventoryRoutes {
 
     @Bean
-    public RouterFunction<ServerResponse> inventoryResponse() {
+    public RouterFunction<ServerResponse> routeToInventory() {
 
         return GatewayRouterFunctions.route("inventory-service")
-                .route(
-                        RequestPredicates.path("/api/v1/inventory/flight/{flightId}"),
-                        request -> request.uri()
-                )
-                .route(
-                        RequestPredicates.path("/api/v1/inventory/zone/{zoneId}"),
-                        request -> forwardWithPathVariable(
-                                request,
-                                "zoneId",
-                                "http://localhost:8080/api/v1/inventory/zone/"
-                        )
-                )
+                .route(path("/api/v1/inventory/**"), http())
+                .before(uri("http://localhost/8080"))
                 .build();
-    }
-
-    private ServerResponse forwardWithPathVariable(ServerRequest request, String pathVariable, String baseUrl) {
-        String value = request.pathVariable(pathVariable);
-        return HandlerFunctions.http(baseUrl + value).handle(request);
     }
 }
